d6311a0b921388856d5743e1bf0f8b00eac5a346
Cresto Lima <crestolima67@gmail.com>
2025-03-17 11:26:35 +0530

    Client Url

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ connectDB().catch(err => {
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173', // Allow your frontend URL
+  origin: process.env.CLIENT_URL || 'https://1-car-4-rentals.netlify.app/', // Allow your frontend URL
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
